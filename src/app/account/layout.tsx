0ce70b8d56cff6f1cdd25133fc30e58160cfbee1
KhaiDucDuong <greenjohnny135@gmail.com>
2024-07-22 21:12:55 +0700

    save work progress

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,14 +1,14 @@
-import Header from "@/components/Header/Header"
+import Image from "next/image";
+
 export default function AccountLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <section>
-        <Header />
-        <div>Layout from Account</div>
-        {children}
-      </section>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="w-[100vw] h-[100vh] flex flex-col justify-center">
+      <div className="min-w-[396px] h-fit m-auto">{children}</div>
+      <div className="absolute top-0 right-0 w-[100vw] h-[100vh] bg-[#201D22] z-[-999]"></div>
+    </section>
+  );
+}
--- src/app/account/sign-in/page.tsx
+++ src/app/account/sign-in/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-    return <p>This is Sign In page</p>;
-  }
+export default function SignInPage() {
+  return <section className="h-[600px] bg-[#2C282E] flex flex-row justify-center max-md:h-[400px]
+  rounded-[15px] ">
+    <div className="w-[400px] border-r-[1px] border-white max-md:hidden p-[8px]">
+      <div className="rounded-[7.5px] bg-yellow-200 size-full ">
+
+      </div>
+    </div>
+    <div className="w-[400px] p-[24px]">
+      <div>
+        <h1>Welcome back</h1>
+        <p>We're so excited to see you again!</p>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  </section>;
+}
