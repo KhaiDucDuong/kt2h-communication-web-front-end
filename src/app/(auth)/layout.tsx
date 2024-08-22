7c08047fab91606a7f99e1c4a20d473b14e55d9a
Dương Đức Khải <greenjohnny135@gmail.com>
2024-08-22 13:09:28 +0700

    fix sign in/ sign up UI responsive bug

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,13 +6,13 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="w-[100vw] h-[100vh] flex flex-col justify-center">
+    <section className="min-w-[100vw] min-h-[100vh] size-full flex flex-col justify-center bg-[#201D22] py-[40px]">
       <Link href={"/"} className="absolute top-[48px] left-[48px] text-[30px] text-white max-sm:top-[24px] max-sm:left-[24px]
       cursor-pointer">
         KT2H
       </Link>
       <div className="w-fit h-fit m-auto">{children}</div>
-      <div className="absolute top-0 right-0 w-[100vw] h-[100vh] bg-[#201D22] z-[-999]"></div>
+      {/* <div className="absolute top-0 right-0 min-w-[100vw] min-h-[100vh] h-[100%] bg-[#201D22] z-[-999]"></div> */}
     </section>
   );
 }
