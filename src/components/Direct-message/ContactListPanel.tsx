20e8b816767cfefcaa9c757e5c76fd90392312e1
KhaiDucDuong <greenjohnny135@gmail.com>
2024-11-10 11:24:34 +0700

    add notification box next to the search bar in direct message tab, fix a user's name display in AcceptFriendRequestNotification

--- src/components/Direct-message/ContactListPanel.tsx
+++ src/components/Direct-message/ContactListPanel.tsx
@@ -1,6 +1,7 @@
 import { Contact } from "@/types/contact";
 import ContactList from "./ContactList";
 import SearchBar from "./SearchBar";
+import Notification from "../Notification/Notification";
 
 interface ContactListPanelProps {
   contacts: Contact[];
@@ -12,11 +13,18 @@ const ContactListPanel = (props: ContactListPanelProps) => {
   return (
     <section className="flex flex-col w-[486px] bg-dark-9 max-lg:w-[330px]">
       <div className="h-[98px] w-[full] ">
-        <SearchBar placeHolder="Find a conversation"/>
+        <div className="h-[98px] flex flex-row">
+          <SearchBar placeHolder="Find a conversation" />
+          <Notification />
+        </div>
         <div className="relative flex w-full border-dark-10 border-[1px] "></div>
       </div>
       <div className="pt-[2px] size-full max-h-[calc(100vh-98px)] px-[7px]">
-        <ContactList contacts={contacts} selectedContact={selectedContact} onSelectContact={onSelectContact} />
+        <ContactList
+          contacts={contacts}
+          selectedContact={selectedContact}
+          onSelectContact={onSelectContact}
+        />
       </div>
     </section>
   );
