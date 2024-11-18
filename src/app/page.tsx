6c89d78b85ee6225936a8133e53e9db75c556881
KhaiDucDuong <greenjohnny135@gmail.com>
2024-11-18 12:15:59 +0700

    add Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+import Home from "@/components/Home/Home";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <div>Home page</div>
-      <Link href={"/sign-in"} className="text-blue-1 hover:underline">Sign in</Link>
-      <Link href={"/dashboard"} className="text-blue-1 hover:underline">Messaging page</Link>
-    </main>
+    // <main className="flex min-h-screen flex-col items-center p-24">
+    //   <div>Home page</div>
+    //   <Link href={"/sign-in"} className="text-blue-1 hover:underline">Sign in</Link>
+    //   <Link href={"/dashboard"} className="text-blue-1 hover:underline">Messaging page</Link>
+    // </main>
+    <Home />
   );
 }
